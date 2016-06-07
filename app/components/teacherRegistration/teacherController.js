aa8856a980bc226803952f9efb1d776c493989e0
yogini.marathe@gmail.com <yogini.marathe@gmail.com>
2016-06-07 16:51:48 +0530

    Removed old code and Added new.
    removed the old code.
    Added new code with proper code structure and index page.

--- app/components/teacherRegistration/teacherController.js
+++ app/components/teacherRegistration/teacherController.js
@@ -6,15 +6,15 @@ taskManagerModule.controller('teacherController', function ($scope,$http) {
 
 	$scope.Register = function(TeacherData) {
 		alert('in reg function');
-		alert($scope.TeacherData.firstName);
-		alert($scope.TeacherData.lastName);
-		alert($scope.TeacherData.email);
-		alert($scope.TeacherData.phone);
-		alert($scope.TeacherData.State);
-		alert($scope.TeacherData.SchoolType.Elem);
-		alert($scope.TeacherData.SchoolType.Secondary);
-		alert( $scope.TeacherData.SchoolType.Highschool);
-		alert( $scope.TeacherData.password);
+//		alert($scope.TeacherData.firstName);
+//		alert($scope.TeacherData.lastName);
+//		alert($scope.TeacherData.email);
+//		alert($scope.TeacherData.phone);
+//		alert($scope.TeacherData.State);
+//		alert($scope.TeacherData.SchoolType.Elem);
+//		alert($scope.TeacherData.SchoolType.Secondary);
+//		alert( $scope.TeacherData.SchoolType.Highschool);
+//		alert( $scope.TeacherData.password);
 		var dataToSend = 
         {
                
@@ -38,6 +38,8 @@ taskManagerModule.controller('teacherController', function ($scope,$http) {
 				 
 				"teacherPassword" : $scope.TeacherData.password
             };
+		
+		
 		//validate the data and formulate post object as understood by Teacher service.
         $http({
           method  : 'POST',
@@ -53,12 +55,33 @@ taskManagerModule.controller('teacherController', function ($scope,$http) {
               $scope.errorUserName = data.errors.username;
               $scope.errorEmail = data.errors.email;
             } else {
-            	
-              $scope.message = data.message;
+            	$scope.teacherID = data.data;
+            	$scope.pill = 2;
+            	angular.element('#pill2').trigger('click');
             }
           });
         };
 
+        $scope.init = function () {
+        	
+        	$scope.pill = 1;
+        	$http({
+                method  : 'GET',
+                url     : 'http://localhost:8012/getStates',
+              
+                headers : {'Content-Type': 'application/json; charset=utf-8'} 
+               })
+                .then(function(data) {
+                  if (data.errors) {
+                    // Showing errors.
+                    $scope.errorName = data.errors.name;
+                    $scope.errorUserName = data.errors.username;
+                    $scope.errorEmail = data.errors.email;
+                  } else {
+                    $scope.States = data.data;
+                  }
+                });
+        };
 //        function to handle Save & Exit click
         $scope.TeacherRegStep1Save = function(TeacherData) {
     		alert('in reg Step1 save function');
